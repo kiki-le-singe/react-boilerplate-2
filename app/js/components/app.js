8250f450c8eb40882fcefa3655e8aeddd4f74526
kiki le singe <aalbertini95@gmail.com>
2015-09-07 13:00:22 +0200

    refactor(app): remove unused components, libs...

--- app/js/components/app.js
+++ app/js/components/app.js
@@ -1,63 +1,15 @@
 import React from 'react';
 import {RouteHandler} from 'react-router';
-import { AppBar, IconButton, Styles } from 'material-ui';
-import AppLeftNav from './app-left-nav';
 
 const {PropTypes, Component} = React;
-// @mui: needs withs material-ui - important
-const ThemeManager = new Styles.ThemeManager();
 
 class App extends Component {
-  // @mui: needs withs material-ui - important
-  getChildContext() {
-    return {
-      muiTheme: ThemeManager.getCurrentTheme()
-    };
-  }
-
-  onLeftIconButtonTouchTap = () => {
-    this.refs.leftNav.toggle();
-  }
-
   render() {
-    let title =
-      this.context.router.isActive('hello') ? 'Hello' :
-      this.context.router.isActive('about') ? 'About' :
-      this.context.router.isActive('my-react-canvas') ? 'My React Canvas' :
-      this.context.router.isActive('tools') ? 'Tools' : '';
-
-    let githubButton = (
-      <IconButton
-        iconClassName="fa fa-github"
-        tooltipPosition="bottom-left"
-        tooltip="Kiki's React Boilerplate"
-        href="https://github.com/kiki-le-singe/react-boilerplate"
-        linkButton={true} />
-    );
-
     return (
-      <div>
-        <AppBar
-          onLeftIconButtonTouchTap={this.onLeftIconButtonTouchTap}
-          title={title}
-          zDepth={0}
-          iconElementRight={githubButton} />
-        <AppLeftNav ref="leftNav" />
-        <div className="content">
-          <RouteHandler />
         </div>
       </div>
     );
   }
 }
-
-App.contextTypes = {
-  router: PropTypes.func
-};
-
-// @mui: needs withs material-ui - important
-App.childContextTypes = {
-  muiTheme: PropTypes.object
-};
 
 export default App;
