1d71e6c67ff8b175694f80c8d93ad565050c419b
kiki le singe <aalbertini95@gmail.com>
2015-09-07 12:53:53 +0200

    refactor(index): remove unused plugin

--- app/js/index.js
+++ app/js/index.js
@@ -2,18 +2,11 @@ import $ from 'jquery';
 import React from 'react';
 import Router from 'react-router';
 import AppRoutes from './app-routes';
-// @mui: needs withs material-ui - important
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import 'font-awesome.scss';
 import 'styles/scss/index.scss';
 
 $(() => {
-  // @mui: needs withs material-ui - important
-  // Needed for onTouchTap. Can go away when react 1.0 release
-  // Check this repo: https://github.com/zilverline/react-tap-event-plugin
-  injectTapEventPlugin();
-
   // https://github.com/rackt/react-router/blob/master/docs/guides/overview.md
   // http://rackt.github.io/react-router/#Router.run
   Router.run(AppRoutes, (Handler) => { // Defaults to `Router.HashLocation`
