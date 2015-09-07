26a23c7a3ddacbe4a539c807c855099df1a9a9c2
kiki le singe <aalbertini95@gmail.com>
2015-09-07 12:55:24 +0200

    feat(index): import necessaries framework7 material css files

--- app/js/index.js
+++ app/js/index.js
@@ -2,7 +2,13 @@ import $ from 'jquery';
 import React from 'react';
 import Router from 'react-router';
 import AppRoutes from './app-routes';
+import 'framework7';
 
+// *** STYLES *** //
+// Path to Framework7 MATERIAL CSS theme styles
+import 'framework7.material.min.css';
+// Path to Framework7 MATERIAL related color styles
+import 'framework7.material.colors.min.css';
 // import 'font-awesome.scss';
 import 'styles/scss/index.scss';
 
