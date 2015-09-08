a577dca98c688b7aa9b347f7b9b5b6d1aa675ad9
kiki le singe <aalbertini95@gmail.com>
2015-09-08 13:49:23 +0200

    feat(about): restructure about page with framework7

--- app/js/components/pages/about/about.js
+++ app/js/components/pages/about/about.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import TopNavBar from '../../top_nav_bar/top-nav-bar';
+
 class About extends React.Component {
   render() {
-    return (<h1>About you</h1>);
+    return (
+      <div data-page="about" className="page">
+        <TopNavBar />
+
+        <div className="page-content">
+          <div className="content-block-title">About</div>
+          <div className="content-block">
+            {/* If no data-panel attribute, Left panel will be opened by default */}
+            <p><a href="#" className="open-panel">Open Left Panel</a></p>
+            {/* Click on link with "open-panel" and data-panel="right" attribute will open Right panel */}
+            <p><a href="#" data-panel="right" className="open-panel">Open Right Panel</a></p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
