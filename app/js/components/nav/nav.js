5547de3ba4a2e46110ce28e06cc32118465a3494
kiki le singe <aalbertini95@gmail.com>
2015-09-08 20:22:31 +0200

    feat(nav): add Nav component

---
+++ app/js/components/nav/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import List from '../list/list';
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <nav>
+        <List />
+      </nav>
+    );
+  }
+}
+
+export default Nav;
