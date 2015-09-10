53ea50196e92de90dd822dd14589f5aaa2f95b65
kiki le singe <aalbertini95@gmail.com>
2015-09-10 21:55:18 +0200

    refactor(list-item): use Link component from react-router

--- app/js/components/list_item/list-item.js
+++ app/js/components/list_item/list-item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 // Docs:
 // - http://www.idangero.us/framework7/docs/list-view.html#list-item-element
@@ -7,12 +8,12 @@ class ListItem extends React.Component {
   render() {
     return (
       <li>
-        <a href="#" className="item-link item-content">
+        <Link to={route} className="item-link item-content">
           <div className="item-media"><i className="icon icon-f7"></i></div>
           <div className="item-inner">
             <div className="item-title">Item title</div>
           </div>
-        </a>
+        </Link>
       </li>
     );
   }
