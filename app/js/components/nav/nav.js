aa41db79a3e0c6ea6bcacb1238fcdd4aca5a4525
kiki le singe <aalbertini95@gmail.com>
2015-09-10 21:49:22 +0200

    feat(nav): pass menuItems data to the List component

--- app/js/components/nav/nav.js
+++ app/js/components/nav/nav.js
@@ -4,9 +4,16 @@ import List from 'components/list/list';
 
 class Nav extends React.Component {
   render() {
+    const menuItems = [
+      { route: 'home', text: 'Home' },
+      { route: 'hello', text: 'Hello' },
+      { route: 'about', text: 'About' },
+      { route: 'tools', text: 'Tools' }
+    ];
+
     return (
       <nav>
-        <List />
+        <List menuItems={menuItems} />
       </nav>
     );
   }
