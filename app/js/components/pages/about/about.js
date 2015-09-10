a4cdfa39f20bcd96d77303dfa880037408ca54fe
kiki le singe <aalbertini95@gmail.com>
2015-09-10 23:49:33 +0200

    refactor(pages): component page extend the Page component

--- app/js/components/pages/about/about.js
+++ app/js/components/pages/about/about.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
-import TopNavBar from 'components/top_nav_bar/top-nav-bar';
+import Page from 'js/components/pages/page';
 
-class About extends React.Component {
-  render() {
+class About extends Page {
+
+  getDataPage() {
+    return 'about';
+  }
+
+  renderPage() {
     return (
-      <div data-page="about" className="page">
-        <TopNavBar />
-
-        <div className="page-content">
-          <div className="content-block-title">About</div>
-          <div className="content-block">
-            {/* If no data-panel attribute, Left panel will be opened by default */}
-            <p><a href="#" className="open-panel">Open Left Panel</a></p>
-            {/* Click on link with "open-panel" and data-panel="right" attribute will open Right panel */}
-            <p><a href="#" data-panel="right" className="open-panel">Open Right Panel</a></p>
-          </div>
+      <div className="page-content">
+        <div className="content-block-title">About</div>
+        <div className="content-block">
+          {/* If no data-panel attribute, Left panel will be opened by default */}
+          <p><a href="#" className="open-panel">Open Left Panel</a></p>
+          {/* Click on link with "open-panel" and data-panel="right" attribute will open Right panel */}
+          <p><a href="#" data-panel="right" className="open-panel">Open Right Panel</a></p>
         </div>
       </div>
     );
--- app/js/components/pages/hello/hello.js
+++ app/js/components/pages/hello/hello.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
-import TopNavBar from 'components/top_nav_bar/top-nav-bar';
+import Page from 'js/components/pages/page';
 
-class Hello extends React.Component {
-  render() {
+class Hello extends Page {
+
+  getDataPage() {
+    return 'hello-world';
+  }
+
+  renderPage() {
     return (
-      <div data-page="hello-world" className="page">
-        <TopNavBar />
-
-        <div className="page-content">
-          <div className="content-block-title">Hello World</div>
-          <div className="content-block">
-            {/* If no data-panel attribute, Left panel will be opened by default */}
-            <p><a href="#" className="open-panel">Open Left Panel</a></p>
-            {/* Click on link with "open-panel" and data-panel="right" attribute will open Right panel */}
-            <p><a href="#" data-panel="right" className="open-panel">Open Right Panel</a></p>
-          </div>
+      <div className="page-content">
+        <div className="content-block-title">Hello World</div>
+        <div className="content-block">
+          {/* If no data-panel attribute, Left panel will be opened by default */}
+          <p><a href="#" className="open-panel">Open Left Panel</a></p>
+          {/* Click on link with "open-panel" and data-panel="right" attribute will open Right panel */}
+          <p><a href="#" data-panel="right" className="open-panel">Open Right Panel</a></p>
         </div>
       </div>
     );
