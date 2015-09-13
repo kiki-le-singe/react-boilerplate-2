14e2a0297f0e28a97636cdf6e0a3533342ff61c6
kiki le singe <aalbertini95@gmail.com>
2015-09-13 19:35:15 +0200

    feat(ToolActions): add fetchAll action

--- src/actions/ToolActions.js
+++ src/actions/ToolActions.js
@@ -1,10 +1,20 @@
 import AppDispatcher from 'dispatcher/AppDispatcher';
 import ToolConstants from 'constants/ToolConstants';
+import Tool from 'services/tool';
 
 // Docs:
 // - https://github.com/facebook/flux/blob/master/examples/flux-todomvc/js/actions/TodoActions.js
 
 export default {
+  fetchAll() {
+    Tool.fetch().then((data) => {
+      AppDispatcher.dispatch({
+        actionType: ToolConstants.TOOL_READ,
+        data: data
+      });
+    });
+  },
+
   delete(id) {
     AppDispatcher.dispatch({
       actionType: ToolConstants.TOOL_DELETE,
