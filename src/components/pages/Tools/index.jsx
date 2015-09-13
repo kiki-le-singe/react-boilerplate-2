f9b378a7390e976fdbf5e3c29ffee54f39e7890b
kiki le singe <aalbertini95@gmail.com>
2015-09-13 18:23:10 +0200

    feat(index): implements the Tool Store

--- src/components/pages/Tools/index.jsx
+++ src/components/pages/Tools/index.jsx
@@ -3,34 +3,64 @@
 
 import React from 'react';
 
+import ToolStore from 'stores/ToolStore';
+
 import Page from 'components/pages/Page';
 import ListSwipeout from 'components/lists/ListSwipeout';
 
+/**
+ * Retrieve the current TOOL data from the ToolStore
+ */
+const getToolState = () => {
+  return {
+    tools: ToolStore.getAll()
+  };
+};
+
 class Tools extends Page {
+
+  // The class' constructor now assumes the role previously filled by componentWillMount
+  constructor(props) {
+    super(props);
+
+    // Warning: getInitialState() is only supported for classes created using React.createClass.
+    // getInitialState() executes exactly once during the lifecycle of the component
+    // and sets up the initial state of the component.
+    // When the state updates, the component re-renders itself.
+    // @see https://facebook.github.io/react/docs/tutorial.html#reactive-state
+    // getInitialState() {
+    //   return {data: []};
+    // }
+    this.state = getToolState();
+  }
+
+  componentDidMount() {
+    super.componentDidMount();
+
+    ToolStore.addChangeListener(this.onChange);
+  }
+
+  componentWillUnmount() {
+    ToolStore.removeChangeListener(this.onChange);
+  }
 
   getDataPage() {
     return 'tools';
   }
 
-  renderPage() {
-    const data = [
-      {
-        title: 'React',
-        text: 'Lorem ipsum ...',
-        route: 'http://facebook.github.io/react/',
-      },
-      {
-        title: 'Flux',
-        text: 'Lorem ipsum ...',
-        route: 'https://facebook.github.io/flux/docs/overview.html',
-      }
-    ];
+  /**
+   * Event handler for 'change' events coming from the TodoStore
+   */
+  onChange = () => {
+    this.setState(getToolState());
+  }
 
+  renderPage() {
     return (
       <div className="page-content">
         <div className="content-block-title">Tools</div>
         <div className="list-block media-list">
-          <ListSwipeout data={data} />
+          <ListSwipeout data={this.state.tools} />
         </div>
       </div>
     );
