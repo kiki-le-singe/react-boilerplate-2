c3dfbd6cc68f32f0ef73bd168abd9aac934e9f74
kiki le singe <aalbertini95@gmail.com>
2015-09-13 23:37:37 +0200

    feat(ListItemSwipeoutTools): add ListItemSwipeoutTools component

---
+++ src/components/tools/ListItemSwipeoutTools.jsx
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react';
+
+import ToolActions from 'actions/ToolActions';
+
+import ListItemSwipeout from 'components/lists/ListItemSwipeout';
+
+// Docs:
+// - https://github.com/facebook/flux/blob/master/examples/flux-todomvc/js/components/TodoItem.react.js
+
+class ListItemSwipeoutTools extends ListItemSwipeout {
+
+  showAlert() {
+    super.showAlert();
+
+    ToolActions.delete(3);
+  }
+}
+
+export default ListItemSwipeoutTools;
