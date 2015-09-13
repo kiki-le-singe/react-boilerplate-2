e2d9d42235c6712cfac49594407e63697230d8b0
kiki le singe <aalbertini95@gmail.com>
2015-09-13 18:14:49 +0200

    feat(ToolConstants): add the tool constants

---
+++ src/constants/ToolConstants.js
@@ -0,0 +1,8 @@
+// https://github.com/facebook/flux/blob/master/examples/flux-todomvc/js/constants/TodoConstants.js
+
+export default {
+  TOOL_CREATE: 'TOOL_CREATE',
+  TOOL_READ: 'TOOL_READ',
+  TOOL_UPDATE: 'TOOL_UPDATE',
+  TOOL_DELETE: 'TOOL_DELETE'
+};
