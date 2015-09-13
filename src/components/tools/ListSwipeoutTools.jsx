a812e43ddb53e02303b7ebd172e70e7ca5e24a54
kiki le singe <aalbertini95@gmail.com>
2015-09-13 23:37:21 +0200

    feat(ListSwipeoutTools): add ListSwipeoutTools component

---
+++ src/components/tools/ListSwipeoutTools.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import ListSwipeout from 'components/lists/ListSwipeout';
+import ListItemSwipeoutTools from './ListItemSwipeoutTools';
+
+class ListSwipeoutTools extends ListSwipeout {
+
+  getDatas() {
+    const { data } = this.props;
+
+    return data.map((item, index) => {
+      return (
+        <ListItemSwipeoutTools route={item.route} title={item.title} text={item.text} key={index} />
+      );
+    });
+  }
+}
+
+export default ListSwipeoutTools;
