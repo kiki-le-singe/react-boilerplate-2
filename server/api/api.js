1ddcc4249b357c4c678e4837f84d512b9794af4f
kiki le singe <aalbertini95@gmail.com>
2015-09-15 15:01:25 +0200

    feat(api): update api

--- server/api/api.js
+++ server/api/api.js
@@ -1,107 +1,122 @@
 exports.tools = [
   {
     id: 1,
-    name: 'HTML5',
-    desc: '5th major revision of the core language of the World Wide Web: the Hypertext Markup Language (HTML).',
-    url: 'http://www.w3.org/TR/html5/',
-    logo: '/images/logos/html5.svg'
+    title: 'HTML5',
+    text: '5th major revision of the core language of the World Wide Web: the Hypertext Markup Language (HTML).',
+    route: 'http://www.w3.org/TR/html5/',
+    logo: '/images/logos/html5.svg',
+    slug: 'html5'
   },
   {
     id: 2,
-    name: 'Bourbon',
-    desc: 'A simple and lightweight mixin library for Sass.',
-    url: 'http://bourbon.io/',
-    logo: '/images/logos/bourbon.svg'
+    title: 'Bourbon',
+    text: 'A simple and lightweight mixin library for Sass.',
+    route: 'http://bourbon.io/',
+    logo: '/images/logos/bourbon.svg',
+    slug: 'bourbon'
   },
   {
     id: 3,
-    name: 'Bourbon NEAT',
-    desc: 'A lightweight semantic grid framework for Sass and Bourbon.',
-    url: 'http://neat.bourbon.io/',
-    logo: '/images/logos/bourbon-neat.svg'
+    title: 'Bourbon NEAT',
+    text: 'A lightweight semantic grid framework for Sass and Bourbon.',
+    route: 'http://neat.bourbon.io/',
+    logo: '/images/logos/bourbon-neat.svg',
+    slug: 'bourbon-neat'
   },
   {
     id: 4,
-    name: 'jQuery',
-    desc: 'jQuery is a fast, small, and feature-rich JavaScript library.',
-    url: 'http://jquery.com/',
-    logo: '/images/logos/jquery.svg'
+    title: 'jQuery',
+    text: 'jQuery is a fast, small, and feature-rich JavaScript library.',
+    route: 'http://jquery.com/',
+    logo: '/images/logos/jquery.svg',
+    slug: 'jquery'
   },
   {
     id: 5,
-    name: 'React',
-    desc: 'A javascript library for building user interfaces.',
-    url: 'https://facebook.github.io/react/index.html',
-    logo: '/images/logos/react.svg'
+    title: 'React',
+    text: 'A javascript library for building user interfaces.',
+    route: 'https://facebook.github.io/react/index.html',
+    logo: '/images/logos/react.svg',
+    slug: 'react'
   },
   {
     id: 6,
-    name: 'Material UI',
-    desc: 'A Set of React Components that Implement Google\'s Material Design.',
-    url: 'http://material-ui.com/',
-    logo: '/images/logos/material-ui.svg'
+    title: 'Material UI',
+    text: 'A Set of React Components that Implement Google\'s Material Design.',
+    route: 'http://material-ui.com/',
+    logo: '/images/logos/material-ui.svg',
+    slug: 'material-ui'
   },
   {
     id: 7,
-    name: 'Babel',
-    desc: 'Babel is a JavaScript compiler. Use next generation JavaScript, today.',
-    url: 'https://babeljs.io/',
-    logo: '/images/logos/babel.svg'
+    title: 'Babel',
+    text: 'Babel is a JavaScript compiler. Use next generation JavaScript, today.',
+    route: 'https://babeljs.io/',
+    logo: '/images/logos/babel.svg',
+    slug: 'babel'
   },
   {
     id: 8,
-    name: 'Lo-Dash',
-    desc: 'A JavaScript utility library delivering consistency, modularity, performance, & extras.',
-    url: 'http://lodash.com/',
-    logo: '/images/logos/lodash.svg'
+    title: 'Lo-Dash',
+    text: 'A JavaScript utility library delivering consistency, modularity, performance, & extras.',
+    route: 'http://lodash.com/',
+    logo: '/images/logos/lodash.svg',
+    slug: 'lo-dash'
   },
   {
     id: 9,
-    name: 'browserify',
-    desc: 'Browserify lets you require(\'modules\') in the browser by bundling up all of your dependencies.',
-    url: 'http://browserify.org/',
-    logo: '/images/logos/browserify.svg'
+    title: 'browserify',
+    text: 'Browserify lets you require(\'modules\') in the browser by bundling up all of your dependencies.',
+    route: 'http://browserify.org/',
+    logo: '/images/logos/browserify.svg',
+    slug: 'browserify'
   },
   {
     id: 10,
-    name: 'Browsersync',
-    desc: 'Browsersync makes your tweaking and testing faster by synchronising file changes and interactions across multiple devices. It’s wicked-fast and totally free.',
-    url: 'http://www.browsersync.io/',
-    logo: null
+    title: 'Browsersync',
+    text: 'Browsersync makes your tweaking and testing faster by synchronising file changes and interactions across multiple devices. It’s wicked-fast and totally free.',
+    route: 'http://www.browsersync.io/',
+    logo: null,
+    slug: 'browsersync'
   },
   {
     id: 11,
-    name: 'Express',
-    desc: 'Fast, unopinionated, minimalist web framework for Node.js',
-    url: 'https://www.npmjs.org/package/express',
-    logo: '/images/logos/express.svg'
+    title: 'Express',
+    text: 'Fast, unopinionated, minimalist web framework for Node.js',
+    route: 'https://www.npmjs.org/package/express',
+    logo: '/images/logos/express.svg',
+    slug: 'express'
   },
   {
     id: 12,
-    name: 'KARMA',
-    desc: 'The main goal for Karma is to bring a productive testing environment to developers.',
-    url: 'http://karma-runner.github.io/',
-    logo: '/images/logos/karma.svg'
+    title: 'KARMA',
+    text: 'The main goal for Karma is to bring a productive testing environment to developers.',
+    route: 'http://karma-runner.github.io/',
+    logo: '/images/logos/karma.svg',
+    slug: 'karma'
   },
   {
     id: 13,
-    name: 'Mocha',
-    desc: 'Mocha is a feature-rich JavaScript test framework running on Node.js and the browser, making asynchronous testing simple and fun.',
-    url: 'http://visionmedia.github.io/mocha/',
-    logo: '/images/logos/mocha.svg'
+    title: 'Mocha',
+    text: 'Mocha is a feature-rich JavaScript test framework running on Node.js and the browser, making asynchronous testing simple and fun.',
+    route: 'http://visionmedia.github.io/mocha/',
+    logo: '/images/logos/mocha.svg',
+    slug: 'mocha'
   },
   {
     id: 14,
-    name: 'Sinon.JS',
-    desc: 'Standalone test spies, stubs and mocks for JavaScript. No dependencies, works with any unit testing framework.',
-    url: 'http://sinonjs.org/',
-    logo: null
+    title: 'Sinon.JS',
+    text: 'Standalone test spies, stubs and mocks for JavaScript. No dependencies, works with any unit testing framework.',
+    route: 'http://sinonjs.org/',
+    logo: null,
+    slug: 'sinonjs'
   },
   {
     id: 15,
-    name: 'chaijs',
-    desc: 'Chai is a BDD / TDD assertion library for node and the browser that can be delightfully paired with any javascript testing framework.',
-    url: 'http://chaijs.com/',
-    logo: '/images/logos/chai.svg'
+    title: 'chaijs',
+    text: 'Chai is a BDD / TDD assertion library for node and the browser that can be delightfully paired with any javascript testing framework.',
+    route: 'http://chaijs.com/',
+    logo: '/images/logos/chai.svg',
+    slug: 'chaijs'
   }
 ];
