8f256885dc5c320fe43149a9e4185951751a2612
kiki le singe <aalbertini95@gmail.com>
2015-09-16 18:18:49 +0200

    feat(index): add FloatingButton component

---
+++ src/components/FloatingButton/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Docs:
+// - http://www.idangero.us/framework7/docs/floating-action-button.html
+
+class FloatingButton extends React.Component {
+  render() {
+    return (
+      <a href="#" className="floating-button color-pink">
+        <i className="icon icon-plus"></i>
+      </a>
+    );
+  }
+}
+
+export default FloatingButton;
