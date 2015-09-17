af97debf865c82f0d0d11d0e48057d809043f995
kiki le singe <aalbertini95@gmail.com>
2015-09-17 20:07:28 +0200

    feat(index): isBackPage allows to define if a page is a subpage or not

--- src/components/TopNavBar/index.jsx
+++ src/components/TopNavBar/index.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 // Docs:
 // - http://www.idangero.us/framework7/docs/navbar.html
 
+const propTypes = {
+  isBackPage: PropTypes.bool
+};
+
+const defaultProps = {
+  isBackPage: false
+};
+
 class TopNavBar extends React.Component {
+
+  renderLeftIcon() {
+    const { isBackPage } = this.props;
+
+    if (isBackPage) {
+      return (
+        <a href="#" className="back link icon-only">
+          <i className="icon icon-back"></i>
+        </a>
+      );
+    }
+
+    return (
+      <a href="#" className="open-panel link icon-only">
+        <i className="icon icon-bars"></i>
+      </a>
+    );
+  }
+
   render() {
     return (
       <div className="navbar">
         <div className="navbar-inner">
           <div className="left">
-            {/* If no data-panel attribute, Left panel will be opened by default */}
-            <a href="#" className="open-panel link icon-only">
-              <i className="icon icon-bars"></i>
-            </a>
+            {this.renderLeftIcon()}
           </div>
           <div className="left">React Boilerplate 2</div>
         </div>
@@ -20,5 +44,8 @@ class TopNavBar extends React.Component {
     );
   }
 }
+
+TopNavBar.propTypes = propTypes;
+TopNavBar.defaultProps = defaultProps;
 
 export default TopNavBar;
