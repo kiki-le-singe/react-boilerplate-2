de9b7dcd9162bc8f2a0f5e55259edbf6a0b0a919
kiki le singe <aalbertini95@gmail.com>
2015-09-17 23:14:23 +0200

    feat(Tool): implements ToolStore

--- src/components/pages/Tools/Tool.jsx
+++ src/components/pages/Tools/Tool.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-// import ToolStore from 'stores/ToolStore';
+import ToolStore from 'stores/ToolStore';
 
 import TopNavBar from 'components/TopNavBar';
 import Page from 'components/pages/Page';
+
+/**
+ * Retrieve the current TOOL data from the ToolStore
+ */
+const getToolState = () => {
+  return {
+    tool: ToolStore.getOne()
+  };
+};
 
 const defaultProps = {
   isPageCached: true
@@ -11,8 +20,40 @@ const defaultProps = {
 
 class Tool extends Page {
 
+  // The class' constructor now assumes the role previously filled by componentWillMount
+  constructor(props) {
+    super(props);
+
+    // Warning: getInitialState() is only supported for classes created using React.createClass.
+    // getInitialState() executes exactly once during the lifecycle of the component
+    // and sets up the initial state of the component.
+    // When the state updates, the component re-renders itself.
+    // @see https://facebook.github.io/react/docs/tutorial.html#reactive-state
+    // getInitialState() {
+    //   return {data: []};
+    // }
+    this.state = getToolState();
+  }
+
+  componentDidMount() {
+    super.componentDidMount();
+
+    ToolStore.addChangeListener(this.onChange);
+  }
+
+  componentWillUnmount() {
+    ToolStore.removeChangeListener(this.onChange);
+  }
+
   getDataPage() {
     return 'tool';
+  }
+
+  /**
+   * Event handler for 'change' events coming from the TodoStore
+   */
+  onChange = () => {
+    this.setState(getToolState());
   }
 
   renderTopNavBar() {
