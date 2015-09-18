95bd7e9a689f4bf3a344fe66270578f96e73c1e4
kiki le singe <aalbertini95@gmail.com>
2015-09-19 00:32:55 +0200

    feat(FloatingButtonTools): add FloatingButtonTools component

---
+++ src/components/tools/FloatingButtonTools.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import FloatingButton from 'components/FloatingButton';
+
+// Docs:
+// - http://www.idangero.us/framework7/docs/floating-action-button.html
+
+class FloatingButtonTools extends React.Component {
+
+  render() {
+    return (
+      <a href="#" data-popup=".popup-create-tool" className="floating-button color-pink open-popup ajax">
+        <i className="icon icon-plus"></i>
+      </a>
+    );
+  }
+}
+
+export default FloatingButtonTools;
