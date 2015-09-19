f273170cd330881603de8f101d39c6d673e929a6
kiki le singe <aalbertini95@gmail.com>
2015-09-19 13:26:27 +0200

    feat(app): Enable material option

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -14,7 +14,8 @@ class App extends Component {
     // http://www.idangero.us/framework7/docs/init-app.html
     this.f7App = new Framework7({
       swipePanel: 'left', // http://www.idangero.us/framework7/docs/side-panels.html#open-panels-with-swipe
-      ajaxLinks: 'a.ajax'
+      ajaxLinks: 'a.ajax',
+      material: true
     });
   }
 
